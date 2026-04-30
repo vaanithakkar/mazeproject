fbead0dafab80a0f4246978e78eb903469dbdc75
vaanithakkar <vthakkar491@myfcc.frederick.edu>
2026-04-30 10:37:55 -0400

    grid reader class cleaned up

--- src/GridReader.java
+++ src/GridReader.java
@@ -1,23 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class GridReader {
-
     public static Maze readMaze(String filename) throws FileNotFoundException{
-
         Scanner file = new Scanner(new File(filename));
-
         String[] lines = new String[50];
         int rowCount=0;
-
         while (file.hasNextLine()) {
             lines[rowCount++] = file.nextLine();
         }
-
         String[] firstRow = lines[0].split(",");
         int colCount = firstRow.length;
-
         for (int i=0; i<rowCount; i++){
             String[] temp = lines[i].split(",");
             if (temp.length != colCount){
@@ -25,20 +18,14 @@ public class GridReader {
                 return null;
             }
         }
-
         Space[][] grid = new Space [rowCount][colCount];
-
         Space start = null;
         Space end = null;
-
         for (int r=0; r<rowCount; r++){
-
             String[] tokens = lines[r].split(",");
-
             for (int c=0; c<colCount; c++){
                 String val = tokens[c];
                 Space s = new Space(val,r,c);
-
                 grid[r][c] = s;
                 if (val.equals("S")) {
                     start = s;
@@ -47,40 +34,29 @@ public class GridReader {
                 }
             }
         }
-
         if (start == null || end == null) {
-            System.out.println("Error: Maze must have exactly on Start and one Exit");
+            System.out.println("Error: Maze must have exactly one Start and one Exit");
             return null;
         }
         for (int r = 0; r<rowCount; r++){
             for (int c=0; c<colCount; c++){
                 Space current = grid[r][c];
                 if (current.isBlocked()) continue;
-
-                //go up
                 if (r > 0 && !grid[r - 1][c].isBlocked()){
                     current.addNeighbor(grid[r - 1][c]);
                 }
-
-                //go down
                 if (r < rowCount - 1 && !grid[r + 1][c].isBlocked()) {
                     current.addNeighbor(grid[r + 1][c]);
                 }
-
-                //go left
                 if (c > 0 && !grid[r][c - 1].isBlocked()) {
                     current.addNeighbor(grid[r][c - 1]);
                 }
-
-                //go right
                 if (c < colCount - 1 && !grid[r][c + 1].isBlocked()) {
                     current.addNeighbor(grid[r][c + 1]);
                 }
             }
         }
-
         Maze maze = new Maze(start, end);
         return maze;
     }
-
 }
